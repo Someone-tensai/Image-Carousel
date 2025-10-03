7e6aeb1b8b11e9f10c0791b144cf19bbdd4c3169
Someone-tensai <paudelsulav5@gmail.com>
2025-10-03 20:30:54 +0545

    Image Carousel

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import "./index.css"
+
+
+let prev_index = 0;
+let dots = document.querySelectorAll(".dot");
+show_image(prev_index);
+
+
+function show_image(current_index)
+{
+
+    if(current_index < 0) current_index = 4;
+    else if(current_index > 4) current_index = 0;
+
+    let images = document.querySelectorAll(".image");
+    images[prev_index].style.display = "none";
+    images[current_index].style.display = "inline";
+    
+    dots[prev_index].style.backgroundColor = "#bbb";
+    dots[current_index].style.backgroundColor = "#717171";
+    
+    prev_index = current_index;
+}
+
+function move_carousel(e)
+{   
+    let dirn = e.target.id;
+    let next_index = prev_index + Number(dirn);
+    show_image(next_index);
+
+}
+let left_button = document.querySelector(".left");
+let right_button = document.querySelector(".right");
+
+left_button.addEventListener("click", move_carousel);
+right_button.addEventListener("click", move_carousel);
+
+for(let i = 0; i < dots.length ; i++)
+{
+    dots[i].addEventListener("click", ()=>show_image(i));
+}
+    
